7043e4d49d3f764e61ce0b3239450e8637e69de7
Hashirkhan24 <khn24202@gmail.com>
2024-04-06 09:18:24 +0530

    Add logo and static searchbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import DesktopLogo from "../../public/airbnb-desktop.png";
+import MobileLogo from "../../public/airbnb-mobile.webp";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="w-full border-b">
+        <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+          <Link href="/">
+            <Image
+              src={DesktopLogo}
+              alt="AirBnB Clone"
+              className="w-32 hidden lg:block"
+            />
+            <Image
+              src={MobileLogo}
+              alt="AirBnB Clone"
+              className="w-12 block lg:hidden"
+            />
+          </Link>
+          <div className="rounded-full border px-5 py-2">
+            <h2>Hello From Search</h2>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;

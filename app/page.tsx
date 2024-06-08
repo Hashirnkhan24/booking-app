402e5612a50d1a43e256fa321bb14603c2932e34
Hashirkhan24 <khn24202@gmail.com>
2024-06-08 13:11:48 +0530

    Create category filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import MapFilterItems from "./components/MapFilterItems";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <Button>Welcome Home</Button>
+      <div className="container mx-auto px-5 lg:px-10">
+        <MapFilterItems />
       </div>
     </>
   );
